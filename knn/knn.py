fed6b0d3c26e2da8f1c74499fce0104fa2d448da
Lyu Cheng <Lyuch000@gmail.com>
2019-03-13 06:02:32 +0800

    ignore idea

--- knn/knn.py
+++ knn/knn.py
@@ -1,17 +1,25 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-import csv
-import numpy
-data = numpy.loadtxt(open("./knn/t4-healthy.csv", "rb"), delimiter=",", skiprows=1)
-neigh = KNeighborsClassifier(n_neighbors=3)
-x = data[:,:-1]
-y = data[:,-1:].astype(int).ravel()
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
-neigh.fit(x_train, y_train)
-
-# Predicted class
-# print(neigh.predict(test))
-
-
-# 3 nearest neighbors
-# print(neigh.kneighbors(test)[1])
+from sklearn.model_selection import KFold
+from sklearn import metrics
+import numpy as np
+data = np.loadtxt(open("./knn/t4-healthy.csv", "rb"), delimiter=",", skiprows=1)
+x = data[:, :-1]
+y = data[:, -1:].astype(int).ravel()
+f1_scores = {}
+for k in range(1, 101):
+    print("processing", k)
+    neigh = KNeighborsClassifier(n_neighbors=k)
+    kf = KFold(n_splits=5)
+    kf.get_n_splits(x)
+    for train_index, test_index in kf.split(x):
+        # print("TRAIN:", train_index, "TEST:", test_index)
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        neigh = KNeighborsClassifier(n_neighbors=k)
+        neigh.fit(x_train, y_train)
+        y_pred = neigh.predict(x_test)
+        f1 = metrics.f1_score(y_test, y_pred)
+        f1_scores[k] = f1
+print(f1_scores)
+top_k_scores = sorted(f1_scores.items(), key=lambda x:x[1], reverse=True)[:10]
+print(top_k_scores)
